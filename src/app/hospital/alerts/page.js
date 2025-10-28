5a1a89f84e9daf5e121c279844a36b56402c1128
Code-With-Devansh <95998107+Code-With-Devansh@users.noreply.github.com>
2025-10-28 07:00:35 +0530

    app complete

--- src/app/hospital/alerts/page.js
+++ src/app/hospital/alerts/page.js
@@ -86,10 +86,10 @@ const Alerts = () => {
     const hours = Math.floor(minutes / 60);
     const days = Math.floor(hours / 24);
 
-    if (days > 0) return `${days} day${days !== 1 ? "s" : ""} ago`;
-    if (hours > 0) return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
-    if (minutes > 0) return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
-    return `${seconds} second${seconds !== 1 ? "s" : ""} ago`;
+    if (days > 0) return `${days} day${days !== 1 ? "s" : ""} `;
+    if (hours > 0) return `${hours} hour${hours !== 1 ? "s" : ""} `;
+    if (minutes > 0) return `${minutes} minute${minutes !== 1 ? "s" : ""} `;
+    return `${seconds} second${seconds !== 1 ? "s" : ""} `;
   };
 
   if (loading) {
